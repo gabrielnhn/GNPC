69a6dc2e7af0fff1d74048937e1451aa86fb9a14
Gabriel Nascarella <ielnascarella@hotmail.com>
2022-10-25 18:22:59 -0300

    add tokens to .Y and .H files

--- compiler.h
+++ compiler.h
@@ -13,14 +13,17 @@
 
 #define TOKEN_SIZE 16
 
-typedef enum symbols {
-  symb_program, symb_var, symb_begin, symb_end,
-  symb_identifier, symb_number,
-  symb_dot, symb_comma, symb_semicolon, symb_colon,
-  symb_assignment, symb_open_parenthesis, symb_close_parenthesis,
+typedef enum symbols
+{
+    symb_program, symb_var, symb_begin, symb_end,
+    symb_identifier, symb_number, symb_dot, symb_comma,
+    symb_semicolon, symb_colon, symb_assignment,
+    symb_open_parenthesis, symb_close_parenthesis, symb_label,
+    symb_type, symb_array, symb_of, symb_procedure,
+    symb_goto, symb_if, symb_then, symb_else,
+    symb_while, symb_do, symb_or, symb_and, symb_not,
+    symb_div, symb_asterisk, symb_plus, symb_minus,
 } symbols;
-
-
 
 /* -------------------------------------------------------------------
  * variáveis globais
@@ -32,11 +35,10 @@ extern int lexical_level;
 extern int desloc;
 extern int nl;
 
-
 /* -------------------------------------------------------------------
  * prototipos globais
  * ------------------------------------------------------------------- */
 
-void generate_code (char*, char*);
+void generate_code(char *, char *);
 int yylex();
 void yyerror(const char *s);
