12ac11d177870c9c8aa43dfe6790d64938b640fc
Gabriel Nascarella <ielnascarella@hotmail.com>
2022-10-25 18:05:07 -0300

    add make_token

---
+++ make_token.py
@@ -0,0 +1,12 @@
+
+new_token = input("token: ")
+
+low = new_token.lower()
+upp = new_token.upper()
+
+print( \
+f"""{low}      {{ symbol = symb_{low};
+          strncpy (token, yytext, TOKEN_SIZE);
+          PRINT(\"{low}  \");
+          return {upp};
+ }}""")
