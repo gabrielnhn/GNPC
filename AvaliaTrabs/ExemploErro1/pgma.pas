6836ed5e231c804cb7617d0c8a222439e7959454
Gabriel Nascarella <ielnascarella@hotmail.com>
2023-02-23 12:07:14 -0300

    remove original files

--- AvaliaTrabs/ExemploErro1/pgma.pas
+++
@@ -1,8 +0,0 @@
-program exemploErro1 (input, output);
-var n, k      : integer;
-begin
-   read (n);
-   read (k);
-   n := n>k;
-   write (n)
-end.
--- AvaliaTrabs/ExemploErro2/pgma.pas
+++
@@ -1,8 +0,0 @@
-program exemploErro2 (input, output);
-var n, k      : integer;
-begin
-   read (n);
-   read (k);
-   if (n+1) then n:=1;
-   write (n)
-end.
